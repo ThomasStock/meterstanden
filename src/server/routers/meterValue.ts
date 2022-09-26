898668f87629ea2e5bd5c2e4256326812f820473
thomas stock <thomasstock1985@gmail.com>
2022-09-26 21:27:10 +0200

    postgresql

--- src/server/routers/meterValue.ts
+++ src/server/routers/meterValue.ts
@@ -63,17 +63,19 @@ export const meterValueRouter = t.router({
   }),
   loadDemoData: t.procedure.mutation(async () => {
     await prisma.meterValue.deleteMany();
-    await prisma.meterValue.createMany({
-      data: [
-        { date: new Date("2020-10-01 00:00:00"), value: "16208" },
-        { date: new Date("2021-10-11 00:00:00"), value: "18362" },
-        { date: new Date("2022-08-29 00:00:00"), value: "20257" },
-        { date: new Date("2022-09-14 15:00:00"), value: "20322.3" },
-        { date: new Date("2022-09-23 00:10:13"), value: "20353.8" }
-      ]
-    });
+    for (const data of demoValues) {
+      await prisma.meterValue.create({ data });
+    }
   })
 });
 
 export type MeterValues = inferProcedureOutput<typeof meterValueRouter["list"]>;
 export type MeterValue = MeterValues[number];
+
+const demoValues = [
+  { date: new Date("2020-10-01 00:00:00"), value: "16208" },
+  { date: new Date("2021-10-11 00:00:00"), value: "18362" },
+  { date: new Date("2022-08-29 00:00:00"), value: "20257" },
+  { date: new Date("2022-09-14 15:00:00"), value: "20322.3" },
+  { date: new Date("2022-09-23 00:10:13"), value: "20353.8" }
+];
