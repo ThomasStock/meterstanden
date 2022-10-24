366dddd17ed90cdfd5a09e579f44bf0eff420292
tstock <thomas.stock@aprimo.com>
2022-10-24 15:22:50 +0200

    entry stuff

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -11,26 +11,43 @@ import { motion } from "framer-motion";
 import React, { useState } from "react";
 import meterUIs, { MeterUI } from "./MeterUI";
 
-const menuSpacerProps: SxProps<Theme> = {
-  height: 70,
-  maxHeight: "10vh"
-};
-
 const Menu = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
+  const menuHeightProps: SxProps<Theme> = {
+    height: { xs: 50, sm: 70 }
+  };
+
+  const entryHeightProps: SxProps<Theme> = {
+    height: { xs: 70, sm: 90 }
+  };
+
   return (
     <>
-      <Box sx={menuSpacerProps}></Box>
+      <Box sx={entryHeightProps}></Box>
+      <Box sx={menuHeightProps}></Box>
+      <Box
+        sx={(theme) => ({
+          position: "fixed",
+          bottom: menuHeightProps.height,
+          ...entryHeightProps,
+          right: 0,
+          left: 0,
+          zIndex: 90,
+          backgroundColor: theme.palette.background.paper
+        })}
+      >
+        entry
+      </Box>
       <Box
         sx={(theme) => ({
           position: "fixed",
           bottom: 0,
           right: -25,
           left: -25,
-          ...menuSpacerProps,
+          ...menuHeightProps,
           zIndex: "100",
-          backgroundColor: theme.palette.secondary.main
+          backgroundColor: theme.palette.background.paper
         })}
       >
         <Stack
@@ -73,7 +90,8 @@ const MenuItem = (props: MenuItemProps) => {
         sx={{
           height: "100%",
           alignItems: "center",
-          justifyContent: "center"
+          justifyContent: "center",
+          transform: "skewX(15deg)"
         }}
       >
         <Icon
@@ -122,14 +140,16 @@ const MenuItem = (props: MenuItemProps) => {
       component={motion.div}
       variants={{
         initial: {
-          transform: "scale(1) skewX(-15deg)"
+          transform: "scale(1) skewX(-15deg)",
+          filter: "drop-shadow(0px 0px 0px rgb(0,0,0,0))"
         },
         selected: {
-          transform: "scale(1.05) skewX(-15deg)",
+          transform: "scale(1) skewX(-15deg)",
+          filter: "drop-shadow(2px 4px 6px rgb(0,0,0,0.3))",
           zIndex: 105
         },
         unselected: {
-          transform: "scale(0.8) skewX(-15deg)",
+          transform: "scale(1) skewX(-15deg)",
           zIndex: 95
         }
       }}
@@ -138,8 +158,7 @@ const MenuItem = (props: MenuItemProps) => {
         backgroundColor: bgColor,
         width: "100%",
         justifyContent: "center",
-        transformOrigin: "bottom",
-        filter: "drop-shadow(2px 4px 6px rgb(0,0,0,0.3))"
+        transformOrigin: "bottom"
       }}
     >
       {isMobile ? renderMobile() : renderDesktop()}
